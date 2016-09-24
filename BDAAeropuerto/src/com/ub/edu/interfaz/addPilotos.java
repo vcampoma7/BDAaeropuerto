fc7896e8f099788823f7a6a6c95589025693a508
vcampoma7 <vcampoma7@alumnes.ub.edu>
2016-09-24 18:19:08 +0200

    Add files via upload

--- BDAAeropuerto/src/com/ub/edu/interfaz/addPilotos.java
+++ BDAAeropuerto/src/com/ub/edu/interfaz/addPilotos.java
@@ -11,7 +11,7 @@ import javax.swing.border.EmptyBorder;
 
 import com.ub.edu.bda.accesosHibernate;
 
-import Objetos.aeropuerto;
+import Objetos.piloto;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -22,21 +22,21 @@ import java.awt.event.ActionEvent;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Window.Type;
 
-public class addAeropuertos extends JDialog {
+public class addPilotos extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField txtNombreAeropuerto;
-	private JTextField txtNombreCiudad;
-	private JTextField txtCodigoInternacional;
-	private JTextField txtCosteHandling;
+	private JTextField txtNombrePiloto;
+	private JTextField txtApellidoPiloto;
+	private JTextField txtHorasDeVuelo;
+	private JTextField txtId_aeropuerto;
 	private boolean editar;
 	accesosHibernate acc = new accesosHibernate();
-	aeropuerto a;
+	piloto a;
 	
 	/**
 	 * Create the dialog.
 	 */
-	public addAeropuertos(JFrame parent, boolean modal, boolean editar, aeropuerto a) {
+	public addPilotos(JFrame parent, boolean modal, boolean editar, piloto a) {
 		super(parent, modal);
 		
 		this.editar = editar;
@@ -51,22 +51,22 @@ public class addAeropuertos extends JDialog {
 		JLabel label;
 		
 		if(editar == false){	
-			label = new JLabel("A\u00F1adir aeropuerto:");
+			label = new JLabel("A\u00F1adir piloto:");
 		}else{
-			label = new JLabel("Editar aeropuerto:");
+			label = new JLabel("Editar piloto:");
 		}
 		JLabel label_1 = new JLabel("Nombre:");
 		JLabel label_2 = new JLabel("Ciudad:");
 		JLabel label_3 = new JLabel("Codigo internacional:");
 		JLabel label_4 = new JLabel("Coste del handling:");
-		txtNombreAeropuerto = new JTextField();
-		txtNombreAeropuerto.setColumns(10);
-		txtNombreCiudad = new JTextField();
-		txtNombreCiudad.setColumns(10);
-		txtCodigoInternacional = new JTextField();
-		txtCodigoInternacional.setColumns(10);
-		txtCosteHandling = new JTextField();
-		txtCosteHandling.setColumns(10);
+		txtNombrePiloto = new JTextField();
+		txtNombrePiloto.setColumns(10);
+		txtApellidoPiloto = new JTextField();
+		txtApellidoPiloto.setColumns(10);
+		txtHorasDeVuelo = new JTextField();
+		txtHorasDeVuelo.setColumns(10);
+		txtId_aeropuerto = new JTextField();
+		txtId_aeropuerto.setColumns(10);
 		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
 		gl_contentPanel.setHorizontalGroup(
 			gl_contentPanel.createParallelGroup(Alignment.LEADING)
@@ -78,21 +78,21 @@ public class addAeropuertos extends JDialog {
 							.addGap(60)
 							.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
-							.addComponent(txtNombreAeropuerto, GroupLayout.PREFERRED_SIZE, 285, GroupLayout.PREFERRED_SIZE))
+							.addComponent(txtNombrePiloto, GroupLayout.PREFERRED_SIZE, 285, GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addGap(64)
 							.addComponent(label_2, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
-							.addComponent(txtNombreCiudad, GroupLayout.PREFERRED_SIZE, 285, GroupLayout.PREFERRED_SIZE))
+							.addComponent(txtApellidoPiloto, GroupLayout.PREFERRED_SIZE, 285, GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addComponent(label_3, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
-							.addComponent(txtCodigoInternacional, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
+							.addComponent(txtHorasDeVuelo, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addGap(9)
 							.addComponent(label_4, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
-							.addComponent(txtCosteHandling, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)))
+							.addComponent(txtId_aeropuerto, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_contentPanel.setVerticalGroup(
@@ -106,25 +106,25 @@ public class addAeropuertos extends JDialog {
 						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addGap(3)
 							.addComponent(label_1))
-						.addComponent(txtNombreAeropuerto, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtNombrePiloto, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addGap(3)
 							.addComponent(label_2))
-						.addComponent(txtNombreCiudad, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtApellidoPiloto, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addGap(3)
 							.addComponent(label_3))
-						.addComponent(txtCodigoInternacional, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtHorasDeVuelo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addGap(3)
 							.addComponent(label_4))
-						.addComponent(txtCosteHandling, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtId_aeropuerto, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(32))
 		);
 		contentPanel.setLayout(gl_contentPanel);
@@ -133,37 +133,36 @@ public class addAeropuertos extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton addAeropuerto = new JButton("Guardar aeropuerto");
-				addAeropuerto.addActionListener(new ActionListener() {
+				JButton addPiloto = new JButton("Guardar piloto");
+				addPiloto.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						
-						String nombre = txtNombreAeropuerto.getText();
-						String ciudad = txtNombreCiudad.getText();
-						String codigo = txtCodigoInternacional.getText();
-						float costeHandling = Float.valueOf(txtCosteHandling.getText());
+						String nombre = txtNombrePiloto.getText();
+						String apellido = txtApellidoPiloto.getText();
+						float horasDeVuelo = Float.valueOf(txtHorasDeVuelo.getText());
+						Integer id_aeropuerto = Integer.valueOf(txtId_aeropuerto.getText());
 						
 						if(editar == false){
 
-							aeropuerto a = new aeropuerto(nombre, ciudad, codigo, costeHandling);
-	
-							acc.insertAeropuerto(a);
+							piloto a = new piloto(nombre, apellido, horasDeVuelo,id_aeropuerto);
+							acc.insertPiloto(a);
 						}
 						else{
 							
-							a.setId_aeropuerto(a.getId_aeropuerto());
+							a.setIdPiloto(a.getIdPiloto());
 							a.setNombre(nombre);
-							a.setCiudad(ciudad);
-							a.setCodigoInternacional(codigo);
-							a.setCosteDelHandling(costeHandling);
-							acc.updateAeropuerto(a);
+							a.setApellido(apellido);
+							a.setHorasDeVuelo(horasDeVuelo);
+							a.setId_aeropuerto(id_aeropuerto);
+							acc.updatePiloto(a);
 						}
 						
 						dispose();
 					}
 				});
-				addAeropuerto.setActionCommand("OK");
-				buttonPane.add(addAeropuerto);
-				getRootPane().setDefaultButton(addAeropuerto);
+				addPiloto.setActionCommand("OK");
+				buttonPane.add(addPiloto);
+				getRootPane().setDefaultButton(addPiloto);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
@@ -178,10 +177,10 @@ public class addAeropuertos extends JDialog {
 		}
 		
 		if(editar==true){
-			txtNombreAeropuerto.setText(a.getNombre());
-			txtNombreCiudad.setText(a.getCiudad());
-			txtCodigoInternacional.setText(a.getcodigoInternacional());
-			txtCosteHandling.setText(a.getCosteDelHandling().toString());
+			txtNombrePiloto.setText(a.getNombre());
+			txtApellidoPiloto.setText(a.getApellido());
+			//txtHorasDeVuelo.setText(a.getHorasDeVuelo().toString());
+			//txtId_aeropuerto.setText(a.getId_aeropuerto().toString());
 		}
 	}
 }
