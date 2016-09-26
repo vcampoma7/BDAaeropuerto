77a5c12e231f115c00868a48931f8b6cf5ad8066
vcampoma7 <vcampoma7@alumnes.ub.edu>
2016-09-26 19:48:10 +0200

    Add files via upload

---
+++ BDAAeropuerto/src/com/ub/edu/interfaz/Login.java
@@ -0,0 +1,165 @@
+package com.ub.edu.interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.ub.edu.bda.accesosHibernate;
+
+import Objetos.Aeropuerto;
+import Objetos.Usuario;
+
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private final Action action = new SwingAction();
+	private JTextField txtUsuario;
+	private JPasswordField jpassClave;
+	accesosHibernate h = new accesosHibernate();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 229, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JButton btnAcceder = new JButton("ACCEDER");
+		btnAcceder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				validar_ingreso();
+			}
+		});
+		btnAcceder.setFont(new Font("Tahoma", Font.BOLD, 11));
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setColumns(10);
+		
+		jpassClave = new JPasswordField();
+		
+		JLabel lblNewLabel = new JLabel("Login");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(34)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblPassword)
+							.addContainerGap(146, Short.MAX_VALUE))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(jpassClave, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE)
+								.addComponent(lblNewLabel, Alignment.LEADING)
+								.addComponent(lblUsuario, GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE)
+								.addComponent(btnAcceder, GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(txtUsuario, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)))
+							.addGap(73))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel)
+					.addGap(37)
+					.addComponent(lblUsuario, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtUsuario, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblPassword, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(jpassClave, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnAcceder, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(38, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+	public void validar_ingreso(){
+		char clave[]=jpassClave.getPassword();
+		String clavedef=new String(clave);
+		String usuario = txtUsuario.getText();
+		
+		
+		accesosHibernate h = new accesosHibernate();
+		@SuppressWarnings("unchecked")
+		List <Usuario> user = h.select("select * FROM usuario where username = " + usuario + " and password = " + clavedef).addEntity(Aeropuerto.class).list();
+	    
+		
+		if (!user.isEmpty()){
+	            this.dispose();
+	
+	            JOptionPane.showMessageDialog(null, "Bienvenido\n"
+	            + "Has ingresado satisfactoriamente al sistema",   "Mensaje de bienvenida",
+	            JOptionPane.INFORMATION_MESSAGE);
+	
+	            MenuPrincipal menuPrincipal = new MenuPrincipal();
+	            menuPrincipal.setVisible(true);
+	
+	
+	    }else {
+	    	JOptionPane.showMessageDialog(null, "Acceso denegado:\n"
+	            + "Por favor ingrese un usuario y/o contraseña correctos", "Acceso denegado",
+	            JOptionPane.ERROR_MESSAGE);
+	    }
+	}
+
+	
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+}
